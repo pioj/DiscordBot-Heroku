ffc5b540f7c385b2c91385ef3db7fc3fb100fd64
pioj <piojete@gmail.com>
2018-08-17 13:02:06 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+// NDc5OTY0NTYxNTIxNTA4MzYz.Dlg69Q.7gMhorvJPdwV_U_IU1GGEAqO124
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+    }
+});
+
+//THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
