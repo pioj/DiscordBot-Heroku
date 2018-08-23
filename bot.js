4f532035780ce9f614bc5cbeede7170667385368
pioj <piojete@gmail.com>
2018-08-23 14:29:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefixes = 'manubot';
+var commands = ['manual', 'help', 'busca', 'dame'];
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
+  
+  var action = 'none';
+  
+  if (message.content.toLowerCase().includes('hola')) {
+    action ='salute';
+  } else {
+    if (message.content.toLowerCase().includes(prefixes)) {
+        action = 'order';
+  }
+  
+  //checkeo de prefijo
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
   if (message.content === 'hola') {
       message.channel.send("Hello " + message.author.toString() + ", and welcome!");
     }
 });
+
 
 //THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
