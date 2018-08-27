8b8e5da5a4628fdc2be5d06a702f81a118adc95c
pioj <piojete@gmail.com>
2018-08-27 19:05:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
 });
-
+ /*
 client.on('message', message => {
   
   var action = 'null';
@@ -42,7 +42,7 @@ client.on('message', message => {
   }
   console.log('last action: ' + message.content);
 });
-
+*/
 
 //THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
